85ed65662488acd53f4224edb6a29f5216ac42b6
Taras Denysenko <terrapass93@gmail.com>
2026-03-21 23:45:45 +0200

    _rendering.default_render_handler is now exposed as part of the public package API.

--- src/kannushi/__init__.py
+++ src/kannushi/__init__.py
@@ -7,6 +7,7 @@ __all__ = [
     "post_process_vars",
     "RenderConfig",
     "RenderDirResult",
+    "default_render_handler",
     "render_dir"
 ]
 
@@ -18,4 +19,4 @@ from . import exceptions, extensions, timing
 from ._vars import TemplateVariables
 from ._vars.loading import load_vars_from_yaml_files
 from ._vars.post_processing import post_process_vars
-from ._rendering import RenderConfig, RenderDirResult, render_dir
+from ._rendering import RenderConfig, RenderDirResult, default_render_handler, render_dir
